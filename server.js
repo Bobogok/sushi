72bc80381008a9693c59323e4ff6db094f5d57b7
Ivan M <vania.morskov@yandex.ru>
2021-10-28 19:25:18 +0300

    add proxy node.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const jsonServer = require('json-server');
+const proxy = require('express-http-proxy');
 
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
@@ -6,6 +7,7 @@ const middlewares = jsonServer.defaults({
   static: './build'
 });
 
+server.use('/items', proxy('https://react-sushi-test.herokuapp.com'));
 server.use(middlewares);
 server.use(router);
 
